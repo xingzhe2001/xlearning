4017abc812526a69445496ca409cb8d120420fcd
Yang <xingzhe2001@gmail.com>
2019-01-01 19:15:41 +0800

    add pre-normalize, and alpha cross test

--- lg_test.py
+++ lg_test.py
@@ -17,14 +17,61 @@ Y1_train = Y1[:-20]
 X1_test = X1[-20:]
 Y1_test = Y1[-20:]
 
-our_rg = us.LinearRegression()
-our_rg.fit(X1_train, Y1_train)
-our_Y1_pred = our_rg.predict(X1_test)
+def test_fit():
 
-# The coefficients
-print('Coefficients: \n', our_rg.coef_)
-# The mean squared error
-print("Mean squared error: %.2f"
-      % mean_squared_error(Y1_test, our_Y1_pred))
-# Explained variance score: 1 is perfect prediction
-print('Variance score: %.2f' % r2_score(Y1_test, our_Y1_pred))
+      print('Test LinearRegression without normalize input, alpha = 0.3:\n')
+      our_rg = us.LinearRegression(normalize=False, alpha=0.3)
+      our_rg.fit(X1_train, Y1_train)
+      our_Y1_pred = our_rg.predict(X1_test)
+
+      # The performance
+      print('Total iterate count: ', our_rg.iter_count)
+      # The coefficients
+      print('Coefficients: \n', our_rg.coef_)
+      # The mean squared error
+      print("Mean squared error: %.2f"
+            % mean_squared_error(Y1_test, our_Y1_pred))
+      # Explained variance score: 1 is perfect prediction
+      print('Variance score: %.2f' % r2_score(Y1_test, our_Y1_pred))
+
+def test_params():
+      iters = []
+      iters_for_normalize = []
+
+      errors = []
+      errors_for_normalize = []
+
+      alphas = np.arange(0.1, 1.1, 0.1)
+
+      for alpha in alphas:
+            our_rg = us.LinearRegression(normalize = False, alpha = alpha)
+            our_rg.fit(X1_train, Y1_train)
+            our_Y1_pred = our_rg.predict(X1_test)
+
+            iters.append(our_rg.iter_count)
+            errors.append(mean_squared_error(Y1_test, our_Y1_pred))
+
+      for alpha in alphas:
+            our_rg = us.LinearRegression(normalize = True, alpha = alpha)
+            our_rg.fit(X1_train, Y1_train)
+            our_Y1_pred = our_rg.predict(X1_test)
+
+            iters_for_normalize.append(our_rg.iter_count)
+            errors_for_normalize.append(mean_squared_error(Y1_test, our_Y1_pred))
+
+      _, ax1 = plt.subplots()
+      ax1.plot(alphas, iters, 'b-')
+      ax1.plot(alphas, iters_for_normalize, 'g-')
+      ax1.set_ylabel('iter_count', color = 'b')
+
+      ax2 = ax1.twinx()
+      ax2.plot(alphas, errors, 'r-')
+      ax2.plot(alphas, errors_for_normalize, 'y-')
+      ax2.set_ylabel('mean sqrt err', color = 'r')
+
+      plt.show()
+
+test_fit()
+test_params()
+
+
--- linear_model.py
+++ linear_model.py
@@ -1,10 +1,17 @@
+#一个首先的线性回归, numpy不熟悉暂时用循环做了些工作
+
 import numpy as np
 
 class LinearRegression:
-    def __init__(self):
+    def __init__(self, normalize = True, alpha=0.3):
         self.coef_ = 0.0
         self.intercept = 0.
         self.theta = None
+        self.normalize = normalize
+        self.offset = 1.0
+        self.scalar = 1.0
+        self.alpha = alpha
+        self.iter_count = 0
         pass
 
     #for n feature, x = 0:n, theta = 0:n
@@ -49,27 +56,53 @@ class LinearRegression:
     def __gradient_descent(self):
         cost = 100000.0
         threshold = 0.01
-        alpha = 0.3
-
 
         #repeat until convergence
         while threshold < cost:
-            self.theta = self.theta - alpha * self.__partialderiv_J_func()
-            self.intercept = self.intercept - alpha * self.__partialderiv_J_func_for_intersect()
+            self.theta = self.theta - self.alpha * self.__partialderiv_J_func()
+            self.intercept = self.intercept - self.alpha * self.__partialderiv_J_func_for_intersect()
             cost = self.__Jfunction()
+            self.iter_count += 1
 
 
+    def __calculate_norm_params(self, x):
+        offset = np.zeros(self.n_feature)
+        scalar = np.ones(self.n_feature)
+        for feature_idx in range(0, self.n_feature):
+            col = x[:, np.newaxis, feature_idx]
+            min = col.min()
+            max = col.max()
+            mean = col.mean()
+
+            if( min != max):
+                scalar[feature_idx] = 1.0/(max - min)
+            else:
+                scalar[feature_idx] = 1.0/max
+            
+            offset[feature_idx] = mean
+
+        return offset, scalar
+
+    def __normalize(self, x):
+        return (x - self.offset) * self.scalar       
 
     
     def fit(self, x, y):
         if x.shape[0] != y.shape[0]:
             raise 'x, y have different length!'
-
-        self.x = x
-        self.y = y        
+     
         self.m = x.shape[0]
+        self.n_feature = x.shape[1]
         self.theta = np.zeros(x[0].size)
 
+        if self.normalize:
+            self.offset, self.scalar = self.__calculate_norm_params(x)
+            self.x = self.__normalize(x)
+        else:
+            self.x = x
+
+        self.y = y 
+        
         self.__gradient_descent()
 
         self.coef_ = self.theta
@@ -78,6 +111,10 @@ class LinearRegression:
     def predict(self, x):
         y_pred = []
         for element in x:
-            y_pred.append(self.__hypothetic(element))
+            xi = element
+            if self.normalize:
+                xi = self.__normalize(element)
+
+            y_pred.append(self.__hypothetic(xi))
 
         return y_pred
